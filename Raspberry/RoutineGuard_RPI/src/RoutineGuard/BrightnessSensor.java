bf3ff9aaf5701a691647a703cc4a059ab5509f7b
Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
2017-06-15 09:34:31 +0200

    Class BrightnessSensor : Add

---
+++ Raspberry/RoutineGuard_RPI/src/RoutineGuard/BrightnessSensor.java
@@ -0,0 +1,31 @@
+/*
+ *
+ */
+package RoutineGuard;
+
+/**
+ * @author Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
+ * @author Dyvia Fleury <dyvia.fleury@etu.univ-lyon1.fr>
+ * @author Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
+ * @author Alexandre Vignand <alexandre.vignand@etu.univ-lyon1.fr>
+ */
+
+public class BrightnessSensor extends Sensor {
+    
+    /* ----- Classe "BrightnessSensor" - Attributs ----- */
+    private double brightness;
+    private double threshold;
+    /* ----- Classe "BrightnessSensor" - Constructeurs ----- */
+    
+    /* ----- Classe "BrightnessSensor" - Méthodes ----- */
+    public double brightnessCalculation(){
+            
+        
+        
+        return 0; /* TMP */
+    }     
+    /* ----- Classe "BrightnessSensor" - Accesseurs ----- */    
+    
+    /* ----- Classe "BrightnessSensor" - Mutateurs ----- */
+    
+}
