e23b20fc6cacd7ff1b98a5e1f2100a6166251b47
Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
2017-06-15 09:29:33 +0200

    Class AccelerometerSensor : Add

---
+++ Raspberry/RoutineGuard_RPI/src/RoutineGuard/AccelerometerSensor.java
@@ -0,0 +1,25 @@
+/*
+ *
+ */
+package RoutineGuard;
+
+/**
+ * @author Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
+ * @author Dyvia Fleury <dyvia.fleury@etu.univ-lyon1.fr>
+ * @author Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
+ * @author Alexandre Vignand <alexandre.vignand@etu.univ-lyon1.fr>
+ */
+
+public class AccelerometerSensor extends Sensor {
+    
+    /* ----- Classe "AccelerometerSensor" - Attributs ----- */
+    
+    /* ----- Classe "AccelerometerSensor" - Constructeurs ----- */
+    
+    /* ----- Classe "AccelerometerSensor" - Méthodes ----- */
+        
+    /* ----- Classe "AccelerometerSensor" - Accesseurs ----- */    
+    
+    /* ----- Classe "AccelerometerSensor" - Mutateurs ----- */
+    
+}
